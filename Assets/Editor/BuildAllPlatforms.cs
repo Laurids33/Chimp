ba15f7242b65789aa374ac89e07e81efb7d77939
Laurids33 <laurids.dechow@gmail.com>
2025-05-03 00:10:26 +0200

    Replace Name in BuildingScript

--- Assets/Editor/BuildAllPlatforms.cs
+++ Assets/Editor/BuildAllPlatforms.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 public class BuildAllPlatforms
 {
     private static string[] scenes = { "Assets/Scenes/SampleScene.unity" }; // Passe das ggf. an
+    static string gameName = "Chimp";
 
     [MenuItem("Build/Build All Targets")]
     public static void BuildAll()
     {
         // Windows Intel 64-bit
-        BuildPipeline.BuildPlayer(scenes, "Builds/Windows64/MyGame.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, $"Builds/Windows64/{gameName}.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
 
         // Windows ARM64
-        BuildPipeline.BuildPlayer(scenes, "Builds/WindowsARM64/MyGame.exe", BuildTarget.StandaloneWindows64, BuildOptions.None); // Hinweis: Unity unterstützt ARM64 nur über zusätzliche Konfiguration oder bestimmte Versionen
+        BuildPipeline.BuildPlayer(scenes, $"Builds/WindowsARM64/{gameName}.exe", BuildTarget.StandaloneWindows64, BuildOptions.None); // Hinweis: Unity unterstützt ARM64 nur über zusätzliche Konfiguration oder bestimmte Versionen
 
         // WebGL Brotli
         PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Brotli;
@@ -23,9 +24,9 @@ public class BuildAllPlatforms
         BuildPipeline.BuildPlayer(scenes, "Builds/WebGL_NoCompression", BuildTarget.WebGL, BuildOptions.None);
 
         // Linux
-        BuildPipeline.BuildPlayer(scenes, "Builds/Linux/MyGame.x86_64", BuildTarget.StandaloneLinux64, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, $"Builds/Linux/{gameName}.x86_64", BuildTarget.StandaloneLinux64, BuildOptions.None);
 
         // macOS
-        BuildPipeline.BuildPlayer(scenes, "Builds/macOS/MyGame.app", BuildTarget.StandaloneOSX, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, $"Builds/macOS/{gameName}.app", BuildTarget.StandaloneOSX, BuildOptions.None);
     }
 }
